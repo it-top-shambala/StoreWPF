bc2c5833b974a00d6626b53cbd70e49bd68599ce
Курицын Алексей <al.curitsin@gmail.com>
2021-11-19 10:00:04 +0500

    DbConnection, DataBase --add Добавлена библиотека для подключения к базе данных.

--- DbConnectionLib/DbConnection.cs
+++ DbConnectionLib/DbConnection.cs
@@ -1,8 +1,17 @@
-﻿using System;
-
-namespace DbConnectionLib
+﻿namespace DbConnectionLib
 {
     public class DbConnection
     {
+        public string Server { get; set; }
+        public string Database { get; set; }
+        public string Uid { get; set; }
+        public string Pwd { get; set; }
+
+        public override string ToString()
+        {
+            //Server=myServerAddress;Database=myDataBase;Uid=myUsername;Pwd=myPassword;
+            return $"Server={Server};Database={Database};Uid={Uid};Pwd={Pwd};";
+        }
+
     }
 }
