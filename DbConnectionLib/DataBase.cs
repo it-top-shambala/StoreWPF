9a0950dc9234663c8c4fe62b51ee45854c84a12b
anst <starininandrey@gmail.com>
2021-11-19 17:05:28 +0300

    Код-ревью с доработкой

--- DbConnectionLib/DataBase.cs
+++ DbConnectionLib/DataBase.cs
@@ -1,8 +1,6 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.IO;
-using System.Text.Json;
-using static System.Text.Json.JsonSerializer;
 
 namespace DbConnectionLib
 {
@@ -12,6 +10,7 @@ namespace DbConnectionLib
 
         private MySqlConnection _db;
         private MySqlCommand _command;
+        
         private DbConnection _connection;
 
         public event Action<string> Info;
@@ -40,46 +39,14 @@ namespace DbConnectionLib
 
         #endregion
 
-        public bool SerializeJson(string path, DbConnection dbConnection)
-        {
-            Info?.Invoke("Создание шаблона JSON для подключения к БД");
-
-            using (var file = new FileStream(path, FileMode.OpenOrCreate))
-            {
-                JsonSerializer.SerializeAsync<DbConnection>(file, dbConnection);
-            }
-
-            Info?.Invoke($"Успешное создание шаблонного файла JSON для подключения к БД");
-            Info?.Invoke($"Шаблонный файл: {path}");
-            return true;
-        }
-
         #region Init
-
-        private bool DeserializeJson(string path)
-        {
-            Info?.Invoke("Получение данных из JSON для подключения к БД");
-            try
-            {
-                using var file = new FileStream(path, FileMode.Open);
-                _connection = DeserializeAsync<DbConnection>(file).Result;
-            }
-            catch (Exception)
-            {
-                Error?.Invoke("Ошибка получения данных из JSON для подключения к БД");
-                return false;
-            }
-
-            Success?.Invoke("Успешное получение данных из JSON для подключения к БД");
-            return true;
-        }
 
         public bool Init(string path)
         {
             Info?.Invoke("Инициализация");
-            var res = DeserializeJson(path);
+            var _connection = DbConnection.DeserializeJson(path);
 
-            if (!res)
+            if (_connection is null)
             {
                 Error?.Invoke("Ошибка инициализации");
                 return false;
@@ -93,19 +60,7 @@ namespace DbConnectionLib
 
         public bool Init()
         {
-            Info?.Invoke("Инициализация");
-            var res = DeserializeJson("db_connection.json");
-
-            if (!res)
-            {
-                Error?.Invoke("Ошибка инициализации");
-                return false;
-            }
-            _db.ConnectionString = _connection.ToString();
-            _command.Connection = _db;
-
-            Error?.Invoke("Ошибка инициализации");
-            return true;
+            return Init("db_connection.json");
         }
 
         #endregion
--- DbConnectionLib/DbConnection.cs
+++ DbConnectionLib/DbConnection.cs
@@ -1,17 +1,60 @@
-﻿namespace DbConnectionLib
+﻿using System;
+using System.IO;
+
+using static System.Text.Json.JsonSerializer;
+
+namespace DbConnectionLib
 {
-    public class DbConnection
+    public record DbConnection
     {
-        public string Server { get; set; }
-        public string Database { get; set; }
-        public string Uid { get; set; }
-        public string Pwd { get; set; }
+        public string Server { get; init; }
+        public string Database { get; init; }
+        public string Uid { get; init; }
+        public string Pwd { get; init; }
+        
+        public static event Action<string> Info;
+        public static event Action<string> Error;
+        public static event Action<string> Success;
 
         public override string ToString()
         {
             //Server=myServerAddress;Database=myDataBase;Uid=myUsername;Pwd=myPassword;
             return $"Server={Server};Database={Database};Uid={Uid};Pwd={Pwd};";
         }
+        
+        public bool SerializeJson(string path)
+        {
+            Info?.Invoke("Создание шаблона JSON для подключения к БД");
 
+            using (var file = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                SerializeAsync(file, this);
+            }
+
+            Info?.Invoke($"Успешное создание шаблонного файла JSON для подключения к БД");
+            Info?.Invoke($"Шаблонный файл: {path}");
+            return true;
+        }
+        
+        public static DbConnection DeserializeJson(string path)
+        {
+            Info?.Invoke("Получение данных из JSON для подключения к БД");
+
+            DbConnection temp;
+            
+            try
+            {
+                using var file = new FileStream(path, FileMode.Open);
+                temp = DeserializeAsync<DbConnection>(file).Result;
+            }
+            catch (Exception)
+            {
+                Error?.Invoke("Ошибка получения данных из JSON для подключения к БД");
+                return null;
+            }
+
+            Success?.Invoke("Успешное получение данных из JSON для подключения к БД");
+            return temp;
+        }
     }
 }
