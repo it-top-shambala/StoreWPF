0d83bafbd3619b9b7a2408302e508ec2d2ce09c8
Aleksej Kuricyn <al.curitsin@gmail.com>
2021-12-19 15:27:29 +0500

    New design --done Создать отдельный XAML-файл с описанием отдельной карточки --готово

---
+++ Store/ProductCardWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Store
+{
+    public partial class ProductCardWindow : Window
+    {
+        public ProductCardWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
